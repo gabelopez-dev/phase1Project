7e2803cb0e456f48b7dfa49ae52d66ad80053038
Gabriel Lopez <gabelopez84@gmail.com>
2021-07-19 23:06:12 -0400

    I added an event

--- script.js
+++ script.js
@@ -19,11 +19,10 @@ document.addEventListener("DOMContentLoaded", () =>  {
     }
 })
 
-       // const jokePTag = document.querySelector(`#joke`)
-
 const newJoke = document.querySelector('#New-Joke')
 const likedJokes = document.querySelector('#Number-of-Likes')
 const likeButton = document.querySelector('#like-joke-button')
+const UnlikeButton = document.querySelector('#Un-like-joke-button')
 
 function displayData(allJokes){
     let dynamicJokes = Math.floor(Math.random() * 5);
@@ -40,34 +39,42 @@ function displayData(allJokes){
 
 function likeButtonFunction(joke){
   console.log(joke.likes+=1 )
-  likedJokes.innerHTML=joke.likes+1
+  let newJoke = joke.likes
+  likedJokes.innerHTML=newJoke
+  let jokeObject = {
+    "likes": newJoke
+   
+  }
+  let reqPackage = {
+    headers: {'content-type': 'application/json'},
+    method: 'PATCH',
+    body: JSON.stringify(jokeObject)
+    }
+    fetch("http://localhost:3000/jokes/"+`${joke.id}`, reqPackage)
+    .then(res => res.json())
+    .then(res => renderUser(res))
+    UnlikeButton.addEventListener("click", UnlikeButtonFunction)
 }
 
-// function createNewUser(userName){
-//     console.log('dont exist so we here')
-//     let newUser = {
-//         name: userName,
-//         // image: 'https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8cGljdHVyZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80'
-//     }
-//     let reqPackage = {
-//         headers: {'content-type': 'application/json'},
-//         method: 'POST',
-//         body: JSON.stringify(newUser)
-//     }
-//     console.log('we are about to create you')
-//     fetch(BASE_URL, reqPackage)
-//     .then(res => res.json())
-//     .then(res => renderUser(res))
-// }
 
-// function saveJoke() {
-//     if (!likedJokes.includes(currentJoke)) {
-//       likedJokes.push(currentJoke)
-//       const li = document.createElement('li')
-//       li.innerText = currentJoke.joke
-//       likedJokesContainer.append(li)
-//       li.addEventListener('click', event => unlikeJoke(event))
-//     } else {
-//       alert('You already have that joke!')
-//     }
-//   }
+function UnlikeButtonFunction(){
+    console.log(likedJokes)
+    
+    likedJokes.innerHTML -= 1
+
+    // console.log(joke.likes-=1 )
+    // let newJoke = joke.likes-1
+    // likedJokes.innerHTML=newJoke
+    // let jokeObject = {
+    //   "likes": newJoke
+     
+    // }
+    // let reqPackage = {
+    //   headers: {'content-type': 'application/json'},
+    //   method: 'PATCH',
+    //   body: JSON.stringify(jokeObject)
+    //   }
+    //   fetch("http://localhost:3000/jokes/"-`${joke.id}`, reqPackage)
+    //   .then(res => res.json())
+    //   .then(res => renderUser(res))
+  }
